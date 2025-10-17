968cdeaadd56717e826e0048bbf8897cbd031fdd
User <olezhka1629@gmail.com>
2025-10-17 13:01:43 +0200

    chore: fixed formatting and added comments
    On-behalf-of: SAP aleh.yarshou@sap.com

--- internal/subroutine/invite.go
+++ internal/subroutine/invite.go
@@ -8,6 +8,7 @@ import (
 	accountv1alpha1 "github.com/platform-mesh/account-operator/api/v1alpha1"
 	"github.com/platform-mesh/golang-commons/controller/lifecycle/runtimeobject"
 	lifecyclesubroutine "github.com/platform-mesh/golang-commons/controller/lifecycle/subroutine"
+	"github.com/rs/zerolog/log"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -24,7 +25,7 @@ import (
 func NewInviteSubroutine(orgsClient client.Client, mgr mcmanager.Manager) *inviteSubroutine {
 	return &inviteSubroutine{
 		orgsClient: orgsClient,
-		mgr: mgr,
+		mgr:        mgr,
 	}
 }
 
@@ -47,7 +48,7 @@ func (w *inviteSubroutine) GetName() string { return "inviteSubroutine" }
 
 func (w *inviteSubroutine) Process(ctx context.Context, instance runtimeobject.RuntimeObject) (ctrl.Result, errors.OperatorError) {
 	lc := instance.(*kcpv1alpha1.LogicalCluster)
-	wsName:= getWorkspaceName(lc)
+	wsName := getWorkspaceName(lc)
 
 	cl, err := w.mgr.ClusterFromContext(ctx)
 	if err != nil {
@@ -55,11 +56,14 @@ func (w *inviteSubroutine) Process(ctx context.Context, instance runtimeobject.R
 	}
 
 	var account accountv1alpha1.Account
+
+	// to find a newly created organiztion account we need to point :root:orgs
 	err = w.orgsClient.Get(ctx, types.NamespacedName{Name: wsName}, &account)
 	if err != nil {
 		return ctrl.Result{}, errors.NewOperatorError(fmt.Errorf("failed to get account resource %w", err), true, true)
 	}
 
+	// the Invite resource is created in :root:orgs:<new org> workspace
 	obj := &v1alpha1.Invite{ObjectMeta: metav1.ObjectMeta{Name: wsName}}
 	_, err = controllerutil.CreateOrUpdate(ctx, cl.GetClient(), obj, func() error {
 		obj.Spec.Email = *account.Spec.Creator
@@ -69,5 +73,6 @@ func (w *inviteSubroutine) Process(ctx context.Context, instance runtimeobject.R
 	if err != nil {
 		return ctrl.Result{}, errors.NewOperatorError(fmt.Errorf("failed to create Invite resource %w", err), true, true)
 	}
+	log.Info().Msg(fmt.Sprintf("Invite resource for %s has been created", obj.Spec.Email))
 	return ctrl.Result{}, nil
 }
