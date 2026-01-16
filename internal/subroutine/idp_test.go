10a8bb8849dbb2e1822e6cbc3de3bfba1c7c56e6
OlegErshov <olezhka1629@gmail.com>
2026-01-16 14:44:42 +0100

    fix: updated tests
    On-behalf-of: SAP aleh.yarshou@sap.com

--- internal/subroutine/idp_test.go
+++ internal/subroutine/idp_test.go
@@ -165,10 +165,13 @@ func TestIDPSubroutine_Process(t *testing.T) {
 				orgsClient.EXPECT().Create(mock.Anything, mock.AnythingOfType("*v1alpha1.IdentityProviderConfiguration")).
 					RunAndReturn(func(_ context.Context, obj client.Object, _ ...client.CreateOption) error {
 						idp := obj.(*secopv1alpha1.IdentityProviderConfiguration)
-						assert.Len(t, idp.Spec.Clients, 1)
+						assert.Len(t, idp.Spec.Clients, 2)
 						assert.Equal(t, "acme", idp.Spec.Clients[0].ClientName)
 						assert.Equal(t, "portal-client-secret-acme-acme", idp.Spec.Clients[0].SecretRef.Name)
 						assert.Equal(t, "default", idp.Spec.Clients[0].SecretRef.Namespace)
+						assert.Equal(t, "kubectl", idp.Spec.Clients[1].ClientName)
+						assert.Equal(t, secopv1alpha1.IdentityProviderClientTypePublic, idp.Spec.Clients[1].ClientType)
+						assert.Equal(t, []string{"http://localhost:8000", "http://localhost:18000"}, idp.Spec.Clients[1].RedirectURIs)
 						idp.Status.Conditions = []metav1.Condition{{Type: "Ready", Status: metav1.ConditionTrue}}
 						return nil
 					}).Once()
@@ -205,10 +208,13 @@ func TestIDPSubroutine_Process(t *testing.T) {
 				orgsClient.EXPECT().Create(mock.Anything, mock.AnythingOfType("*v1alpha1.IdentityProviderConfiguration")).
 					RunAndReturn(func(_ context.Context, obj client.Object, _ ...client.CreateOption) error {
 						idp := obj.(*secopv1alpha1.IdentityProviderConfiguration)
-						assert.Len(t, idp.Spec.Clients, 1)
+						assert.Len(t, idp.Spec.Clients, 2)
 						assert.Equal(t, "beta", idp.Spec.Clients[0].ClientName)
 						assert.Equal(t, "portal-client-secret-beta-beta", idp.Spec.Clients[0].SecretRef.Name)
 						assert.Equal(t, "default", idp.Spec.Clients[0].SecretRef.Namespace)
+						assert.Equal(t, "kubectl", idp.Spec.Clients[1].ClientName)
+						assert.Equal(t, secopv1alpha1.IdentityProviderClientTypePublic, idp.Spec.Clients[1].ClientType)
+						assert.Equal(t, []string{"http://localhost:8000", "http://localhost:18000"}, idp.Spec.Clients[1].RedirectURIs)
 						idp.Status.Conditions = []metav1.Condition{{Type: "Ready", Status: metav1.ConditionFalse}}
 						return nil
 					}).Once()
