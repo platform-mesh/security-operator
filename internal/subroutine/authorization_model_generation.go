7e7cb453a5a30c1fb614ae780784a23346ddb1bf
OlegErshov <olezhka1629@gmail.com>
2026-01-30 11:06:09 +0100

    fix: added a condition to do not set finalizer on system apibindings
    On-behalf-of: SAP aleh.yarshou@sap.com

--- internal/subroutine/authorization_model_generation.go
+++ internal/subroutine/authorization_model_generation.go
@@ -188,7 +188,11 @@ func (a *AuthorizationModelGenerationSubroutine) Finalize(ctx context.Context, i
 }
 
 // Finalizers implements lifecycle.Subroutine.
-func (a *AuthorizationModelGenerationSubroutine) Finalizers(_ lifecyclecontrollerruntime.RuntimeObject) []string {
+func (a *AuthorizationModelGenerationSubroutine) Finalizers(instance lifecyclecontrollerruntime.RuntimeObject) []string {
+	binding := instance.(*kcpapisv1alpha1.APIBinding)
+	if strings.Contains(binding.Name, "platform-mesh.io") || strings.Contains(binding.Name, "kcp.io") {
+		return []string{}
+	}
 	return []string{"core.platform-mesh.io/apibinding-finalizer"}
 }
 
