6dec10d6982e61e358b33c6e3ca7ce0ffce59eeb
Simon Tesar <simon@simontesar.com>
2026-02-24 07:30:18 +0000

    feat: tuples: render user and objects via separate functions
    On-behalf-of: @SAP <simon@simontesar.com>

--- pkg/fga/tuples.go
+++ pkg/fga/tuples.go
@@ -17,9 +17,9 @@ func InitialTuplesForAccount(acc accountv1alpha1.Account, ai accountv1alpha1.Acc
 		return nil, err
 	}
 	tuples := append(base, v1alpha1.Tuple{
-		User:     fmt.Sprintf("%s:%s/%s", objectType, ai.Spec.ParentAccount.OriginClusterId, ai.Spec.ParentAccount.Name),
+		User:     renderAccountEntity(objectType, ai.Spec.ParentAccount.OriginClusterId, ai.Spec.ParentAccount.Name),
 		Relation: parentRelation,
-		Object:   fmt.Sprintf("%s:%s/%s", objectType, ai.Spec.Account.OriginClusterId, ai.Spec.Account.Name),
+		Object:   renderAccountEntity(objectType, ai.Spec.Account.OriginClusterId, ai.Spec.Account.Name),
 	})
 	return tuples, nil
 }
@@ -45,14 +45,14 @@ func baseTuples(acc accountv1alpha1.Account, ai accountv1alpha1.AccountInfo, cre
 
 	return []v1alpha1.Tuple{
 		{
-			User:     fmt.Sprintf("user:%s", formatUser(*acc.Spec.Creator)),
+			User:     renderCreatorUser(*acc.Spec.Creator),
 			Relation: "assignee",
-			Object:   fmt.Sprintf("role:%s/%s/%s/owner", objectType, ai.Spec.Account.OriginClusterId, ai.Spec.Account.Name),
+			Object:   renderOwnerRole(objectType, ai.Spec.Account.OriginClusterId, ai.Spec.Account.Name),
 		},
 		{
-			User:     fmt.Sprintf("role:%s/%s/%s/owner#assignee", objectType, ai.Spec.Account.OriginClusterId, ai.Spec.Account.Name),
+			User:     renderOwnerRoleAssigneeGroup(objectType, ai.Spec.Account.OriginClusterId, ai.Spec.Account.Name),
 			Relation: creatorRelation,
-			Object:   fmt.Sprintf("%s:%s/%s", objectType, ai.Spec.Account.OriginClusterId, ai.Spec.Account.Name),
+			Object:   renderAccountEntity(objectType, ai.Spec.Account.OriginClusterId, ai.Spec.Account.Name),
 		},
 	}, nil
 }
@@ -61,4 +61,20 @@ func baseTuples(acc accountv1alpha1.Account, ai accountv1alpha1.AccountInfo, cre
 // with dots.
 func formatUser(user string) string {
 	return strings.ReplaceAll(user, ":", ".")
+}
+
+func renderAccountEntity(objectType, originClusterID, name string) string {
+	return fmt.Sprintf("%s:%s/%s", objectType, originClusterID, name)
+}
+
+func renderCreatorUser(creator string) string {
+	return fmt.Sprintf("user:%s", formatUser(creator))
+}
+
+func renderOwnerRole(objectType, originClusterID, name string) string {
+	return fmt.Sprintf("role:%s/%s/%s/owner", objectType, originClusterID, name)
+}
+
+func renderOwnerRoleAssigneeGroup(objectType, originClusterID, name string) string {
+	return fmt.Sprintf("role:%s/%s/%s/owner#assignee", objectType, originClusterID, name)
 }
