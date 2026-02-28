6aa2c896047bffe144116dd2726bb598ca6dda24
OlegErshov <olezhka1629@gmail.com>
2026-02-28 13:54:20 +0100

    chore: remove unused getAllClient function
    On-behalf-of: SAP aleh.yarshou@sap.com

--- internal/controller/apibinding_controller.go
+++ internal/controller/apibinding_controller.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"context"
-	"net/url"
 
 	platformeshconfig "github.com/platform-mesh/golang-commons/config"
 	"github.com/platform-mesh/golang-commons/controller/lifecycle/builder"
@@ -13,69 +12,13 @@ import (
 	"github.com/platform-mesh/security-operator/internal/subroutine"
 	"github.com/rs/zerolog/log"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	mccontext "sigs.k8s.io/multicluster-runtime/pkg/context"
 	mcmanager "sigs.k8s.io/multicluster-runtime/pkg/manager"
 	mcreconcile "sigs.k8s.io/multicluster-runtime/pkg/reconcile"
 
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/rest"
-
-	"github.com/kcp-dev/logicalcluster/v3"
-	kcpapisv1alpha1 "github.com/kcp-dev/sdk/apis/apis/v1alpha1"
 	kcpapisv1alpha2 "github.com/kcp-dev/sdk/apis/apis/v1alpha2"
 )
-
-func GetAllClient(config *rest.Config, schema *runtime.Scheme) (client.Client, error) {
-	allCfg := rest.CopyConfig(config)
-
-	platformMeshClient, err := client.New(allCfg, client.Options{
-		Scheme: schema,
-	})
-	if err != nil {
-		log.Error().Err(err).Msg("unable to create client from config")
-		return nil, err
-	}
-
-	var apiExportEndpointSlice kcpapisv1alpha1.APIExportEndpointSlice
-	err = platformMeshClient.Get(context.Background(), types.NamespacedName{Name: "core.platform-mesh.io"}, &apiExportEndpointSlice)
-	if err != nil {
-		log.Error().Err(err).Msg("unable to get APIExportEndpointSlice")
-		return nil, err
-	}
-
-	virtualWorkspaceUrl, err := url.Parse(apiExportEndpointSlice.Status.APIExportEndpoints[0].URL)
-	if err != nil {
-		log.Error().Err(err).Msg("unable to parse virtual workspace URL")
-		return nil, err
-	}
-
-	parsed, err := url.Parse(allCfg.Host)
-	if err != nil {
-		log.Error().Err(err).Msg("unable to parse host from config")
-		return nil, err
-	}
-
-	parsed.Path, err = url.JoinPath(virtualWorkspaceUrl.Path, "clusters", logicalcluster.Wildcard.String())
-	if err != nil {
-		log.Error().Err(err).Msg("unable to join path")
-		return nil, err
-	}
-
-	allCfg.Host = parsed.String()
-
-	log.Info().Str("host", allCfg.Host).Msg("using host")
-
-	allClient, err := client.New(allCfg, client.Options{
-		Scheme: schema,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return allClient, nil
-}
 
 func NewAPIBindingReconciler(ctx context.Context, logger *logger.Logger, mcMgr mcmanager.Manager) *APIBindingReconciler {
 	allclient, err := iclient.NewForAllPlatformMeshResources(ctx, mcMgr.GetLocalManager().GetConfig(), mcMgr.GetLocalManager().GetScheme())
