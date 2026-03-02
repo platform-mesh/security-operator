bcdbb7ce1c646fbb6224e6a47ec0b96a6911d17c
OlegErshov <olezhka1629@gmail.com>
2026-03-02 10:20:25 +0100

    chore: refactor predicate
    On-behalf-of: SAP aleh.yarshou@sap.com

--- internal/predicates/initializer.go
+++ internal/predicates/initializer.go
@@ -1,9 +1,10 @@
 package predicates
 
 import (
+	"fmt"
 	"slices"
 
-	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	kcpcorev1alpha1 "github.com/kcp-dev/sdk/apis/core/v1alpha1"
@@ -11,24 +12,13 @@ import (
 
 func HasInitializerPredicate(initializerName string) predicate.Predicate {
 	initializer := kcpcorev1alpha1.LogicalClusterInitializer(initializerName)
-	return predicate.Funcs{
-		CreateFunc: func(e event.CreateEvent) bool {
-			lc := e.Object.(*kcpcorev1alpha1.LogicalCluster)
-			return shouldReconcile(lc, initializer)
-		},
-		UpdateFunc: func(e event.UpdateEvent) bool {
-			newLC := e.ObjectNew.(*kcpcorev1alpha1.LogicalCluster)
-			return shouldReconcile(newLC, initializer)
-		},
-		DeleteFunc: func(e event.DeleteEvent) bool {
-			lc := e.Object.(*kcpcorev1alpha1.LogicalCluster)
-			return shouldReconcile(lc, initializer)
-		},
-		GenericFunc: func(e event.GenericEvent) bool {
-			lc := e.Object.(*kcpcorev1alpha1.LogicalCluster)
-			return shouldReconcile(lc, initializer)
-		},
-	}
+	return predicate.NewPredicateFuncs(func(object client.Object) bool {
+		lc, ok := object.(*kcpcorev1alpha1.LogicalCluster)
+		if !ok {
+			panic(fmt.Errorf("received non-LogicalCluster resource in LogicalClusterIsAccountTypeOrg predicate"))
+		}
+		return shouldReconcile(lc, initializer)
+	})
 }
 
 func shouldReconcile(lc *kcpcorev1alpha1.LogicalCluster, initializer kcpcorev1alpha1.LogicalClusterInitializer) bool {
