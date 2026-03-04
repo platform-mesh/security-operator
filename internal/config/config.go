4b79fb20428013fb257cfc35d5282ef8cfc8ed66
makdeniss <denm15221@gmail.com>
2026-03-04 16:33:24 +0200

    Feat/migrate goland commons config (#380)
    * feat: migrate to new golang-commons config API (#363)
    
    * feat: migrate to new golang-commons config API (#363)
    
    * feat: migrate to new golang-commons config API (#363)
    
    * feat: migrate to new golang-commons config API

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,76 +3,76 @@ package config
 import "github.com/spf13/pflag"
 
 type InviteConfig struct {
-	KeycloakBaseURL      string `mapstructure:"invite-keycloak-base-url"`
-	KeycloakClientID     string `mapstructure:"invite-keycloak-client-id" default:"security-operator"`
-	KeycloakClientSecret string `mapstructure:"invite-keycloak-client-secret"`
+	KeycloakBaseURL      string
+	KeycloakClientID     string
+	KeycloakClientSecret string
 }
 
 type WebhooksConfig struct {
-	Enabled bool   `mapstructure:"webhooks-enabled" default:"false"`
-	Port    int    `mapstructure:"webhooks-port" default:"9443"`
-	CertDir string `mapstructure:"webhooks-cert-dir" default:"/tmp/k8s-webhook-server/serving-certs"`
+	Enabled bool
+	Port    int
+	CertDir string
 }
 
 type InitializerConfig struct {
-	WorkspaceInitializerEnabled bool `mapstructure:"initializer-workspace-enabled" default:"true"`
-	IDPEnabled                  bool `mapstructure:"initializer-idp-enabled" default:"true"`
-	InviteEnabled               bool `mapstructure:"initializer-invite-enabled" default:"true"`
-	WorkspaceAuthEnabled        bool `mapstructure:"initializer-workspace-auth-enabled" default:"true"`
+	WorkspaceInitializerEnabled bool
+	IDPEnabled                  bool
+	InviteEnabled               bool
+	WorkspaceAuthEnabled        bool
 }
 
 type FGAConfig struct {
-	Target          string `mapstructure:"fga-target"`
-	ObjectType      string `mapstructure:"fga-object-type" default:"core_platform-mesh_io_account"`
-	ParentRelation  string `mapstructure:"fga-parent-relation" default:"parent"`
-	CreatorRelation string `mapstructure:"fga-creator-relation" default:"owner"`
+	Target          string
+	ObjectType      string
+	ParentRelation  string
+	CreatorRelation string
 }
 
 type KCPConfig struct {
-	Kubeconfig string `mapstructure:"kcp-kubeconfig" default:"/api-kubeconfig/kubeconfig"`
+	Kubeconfig string
 }
 
 type IDPConfig struct {
-	RealmDenyList []string `mapstructure:"idp-realm-deny-list"`
+	RealmDenyList []string
 
-	SMTPServer  string `mapstructure:"idp-smtp-server"`
-	SMTPPort    int    `mapstructure:"idp-smtp-port"`
-	FromAddress string `mapstructure:"idp-from-address"`
+	SMTPServer  string
+	SMTPPort    int
+	FromAddress string
 
-	SSL      bool `mapstructure:"idp-smtp-ssl" default:"false"`
-	StartTLS bool `mapstructure:"idp-smtp-starttls" default:"false"`
+	SSL      bool
+	StartTLS bool
 
-	SMTPUser     string `mapstructure:"idp-smtp-user"`
-	SMTPPassword string `mapstructure:"idp-smtp-password"`
+	SMTPUser     string
+	SMTPPassword string
 
-	AdditionalRedirectURLs    []string `mapstructure:"idp-additional-redirect-urls"`
-	KubectlClientRedirectURLs []string `mapstructure:"idp-kubectl-client-redirect-urls" default:"http://localhost:8000,http://localhost:18000"`
+	AdditionalRedirectURLs    []string
+	KubectlClientRedirectURLs []string
 
-	AccessTokenLifespan int  `mapstructure:"idp-access-token-lifespan" default:"28800"`
-	RegistrationAllowed bool `mapstructure:"idp-registration-allowed" default:"false"`
+	AccessTokenLifespan int
+	RegistrationAllowed bool
 }
 
 // Config struct to hold the app config
 type Config struct {
-	FGA                              FGAConfig         `mapstructure:",squash"`
-	KCP                              KCPConfig         `mapstructure:",squash"`
-	APIExportEndpointSliceName       string            `mapstructure:"api-export-endpoint-slice-name" default:"core.platform-mesh.io"`
-	CoreModulePath                   string            `mapstructure:"core-module-path"`
-	BaseDomain                       string            `mapstructure:"base-domain" default:"portal.dev.local:8443"`
-	GroupClaim                       string            `mapstructure:"group-claim" default:"groups"`
-	UserClaim                        string            `mapstructure:"user-claim" default:"email"`
-	DevelopmentAllowUnverifiedEmails bool              `mapstructure:"development-allow-unverified-emails" default:"false"`
-	WorkspacePath                    string            `mapstructure:"workspace-path" default:"root"`
-	WorkspaceTypeName                string            `mapstructure:"workspace-type-name" default:"security"`
-	DomainCALookup                   bool              `mapstructure:"domain-ca-lookup" default:"false"`
-	MigrateAuthorizationModels       bool              `mapstructure:"migrate-authorization-models" default:"false"`
-	HttpClientTimeoutSeconds         int               `mapstructure:"http-client-timeout-seconds" default:"30"`
-	SetDefaultPassword               bool              `mapstructure:"set-default-password" default:"false"`
-	AllowMemberTuplesEnabled         bool              `mapstructure:"allow-member-tuples-enabled" default:"false"`
-	IDP                              IDPConfig         `mapstructure:",squash"`
-	Invite                           InviteConfig      `mapstructure:",squash"`
-	Initializer                      InitializerConfig `mapstructure:",squash"`
-	Webhooks                         WebhooksConfig    `mapstructure:",squash"`
+	FGA                              FGAConfig
+	KCP                              KCPConfig
+	APIExportEndpointSliceName       string
+	CoreModulePath                   string
+	BaseDomain                       string
+	GroupClaim                       string
+	UserClaim                        string
+	DevelopmentAllowUnverifiedEmails bool
+	WorkspacePath                    string
+	WorkspaceTypeName                string
+	DomainCALookup                   bool
+	MigrateAuthorizationModels       bool
+	HttpClientTimeoutSeconds         int
+	SetDefaultPassword               bool
+	AllowMemberTuplesEnabled         bool
+	IDP                              IDPConfig
+	Invite                           InviteConfig
+	Initializer                      InitializerConfig
+	Webhooks                         WebhooksConfig
 }
 
 func NewConfig() Config {
