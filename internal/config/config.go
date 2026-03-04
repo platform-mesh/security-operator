5914b580b4084bad345f1283d5764119941799e2
makdeniss <denm15221@gmail.com>
2026-03-04 11:47:57 +0200

    feat: migrate to new golang-commons config API (#363)

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,80 +21,95 @@ type InitializerConfig struct {
 	WorkspaceAuthEnabled        bool `mapstructure:"initializer-workspace-auth-enabled" default:"true"`
 }
 
+type FGAConfig struct {
+	Target          string `mapstructure:"fga-target"`
+	ObjectType      string `mapstructure:"fga-object-type" default:"core_platform-mesh_io_account"`
+	ParentRelation  string `mapstructure:"fga-parent-relation" default:"parent"`
+	CreatorRelation string `mapstructure:"fga-creator-relation" default:"owner"`
+}
+
+type KCPConfig struct {
+	Kubeconfig string `mapstructure:"kcp-kubeconfig" default:"/api-kubeconfig/kubeconfig"`
+}
+
+type IDPConfig struct {
+	RealmDenyList []string `mapstructure:"idp-realm-deny-list"`
+
+	SMTPServer  string `mapstructure:"idp-smtp-server"`
+	SMTPPort    int    `mapstructure:"idp-smtp-port"`
+	FromAddress string `mapstructure:"idp-from-address"`
+
+	SSL      bool `mapstructure:"idp-smtp-ssl" default:"false"`
+	StartTLS bool `mapstructure:"idp-smtp-starttls" default:"false"`
+
+	SMTPUser     string `mapstructure:"idp-smtp-user"`
+	SMTPPassword string `mapstructure:"idp-smtp-password"`
+
+	AdditionalRedirectURLs    []string `mapstructure:"idp-additional-redirect-urls"`
+	KubectlClientRedirectURLs []string `mapstructure:"idp-kubectl-client-redirect-urls" default:"http://localhost:8000,http://localhost:18000"`
+
+	AccessTokenLifespan int  `mapstructure:"idp-access-token-lifespan" default:"28800"`
+	RegistrationAllowed bool `mapstructure:"idp-registration-allowed" default:"false"`
+}
+
 // Config struct to hold the app config
 type Config struct {
-	FGA struct {
-		Target          string `mapstructure:"fga-target"`
-		ObjectType      string `mapstructure:"fga-object-type" default:"core_platform-mesh_io_account"`
-		ParentRelation  string `mapstructure:"fga-parent-relation" default:"parent"`
-		CreatorRelation string `mapstructure:"fga-creator-relation" default:"owner"`
-	} `mapstructure:",squash"`
-	KCP struct {
-		Kubeconfig string `mapstructure:"kcp-kubeconfig" default:"/api-kubeconfig/kubeconfig"`
-	} `mapstructure:",squash"`
-	APIExportEndpointSliceName       string `mapstructure:"api-export-endpoint-slice-name" default:"core.platform-mesh.io"`
-	CoreModulePath                   string `mapstructure:"core-module-path"`
-	BaseDomain                       string `mapstructure:"base-domain" default:"portal.dev.local:8443"`
-	GroupClaim                       string `mapstructure:"group-claim" default:"groups"`
-	UserClaim                        string `mapstructure:"user-claim" default:"email"`
-	DevelopmentAllowUnverifiedEmails bool   `mapstructure:"development-allow-unverified-emails" default:"false"`
-	WorkspacePath                    string `mapstructure:"workspace-path" default:"root"`
-	WorkspaceTypeName                string `mapstructure:"workspace-type-name" default:"security"`
-	DomainCALookup                   bool   `mapstructure:"domain-ca-lookup" default:"false"`
-	MigrateAuthorizationModels       bool   `mapstructure:"migrate-authorization-models" default:"false"`
-	HttpClientTimeoutSeconds         int    `mapstructure:"http-client-timeout-seconds" default:"30"`
-	SetDefaultPassword               bool   `mapstructure:"set-default-password" default:"false"`
-	AllowMemberTuplesEnabled         bool   `mapstructure:"allow-member-tuples-enabled" default:"false"`
-	IDP                              struct {
-		RealmDenyList []string `mapstructure:"idp-realm-deny-list"`
-
-		// SMTP settings
-		SMTPServer  string `mapstructure:"idp-smtp-server"`
-		SMTPPort    int    `mapstructure:"idp-smtp-port"`
-		FromAddress string `mapstructure:"idp-from-address"`
-
-		// SSL settings
-		SSL      bool `mapstructure:"idp-smtp-ssl" default:"false"`
-		StartTLS bool `mapstructure:"idp-smtp-starttls" default:"false"`
-
-		// Auth settings
-		SMTPUser     string `mapstructure:"idp-smtp-user"`
-		SMTPPassword string `mapstructure:"idp-smtp-password"`
-
-		AdditionalRedirectURLs    []string `mapstructure:"idp-additional-redirect-urls"`
-		KubectlClientRedirectURLs []string `mapstructure:"idp-kubectl-client-redirect-urls" default:"http://localhost:8000,http://localhost:18000"`
-
-		AccessTokenLifespan int  `mapstructure:"idp-access-token-lifespan" default:"28800"`
-		RegistrationAllowed bool `mapstructure:"idp-registration-allowed" default:"false"`
-	} `mapstructure:",squash"`
-	Invite      InviteConfig      `mapstructure:",squash"`
-	Initializer InitializerConfig `mapstructure:",squash"`
-	Webhooks    WebhooksConfig    `mapstructure:",squash"`
+	FGA                              FGAConfig         `mapstructure:",squash"`
+	KCP                              KCPConfig         `mapstructure:",squash"`
+	APIExportEndpointSliceName       string            `mapstructure:"api-export-endpoint-slice-name" default:"core.platform-mesh.io"`
+	CoreModulePath                   string            `mapstructure:"core-module-path"`
+	BaseDomain                       string            `mapstructure:"base-domain" default:"portal.dev.local:8443"`
+	GroupClaim                       string            `mapstructure:"group-claim" default:"groups"`
+	UserClaim                        string            `mapstructure:"user-claim" default:"email"`
+	DevelopmentAllowUnverifiedEmails bool              `mapstructure:"development-allow-unverified-emails" default:"false"`
+	WorkspacePath                    string            `mapstructure:"workspace-path" default:"root"`
+	WorkspaceTypeName                string            `mapstructure:"workspace-type-name" default:"security"`
+	DomainCALookup                   bool              `mapstructure:"domain-ca-lookup" default:"false"`
+	MigrateAuthorizationModels       bool              `mapstructure:"migrate-authorization-models" default:"false"`
+	HttpClientTimeoutSeconds         int               `mapstructure:"http-client-timeout-seconds" default:"30"`
+	SetDefaultPassword               bool              `mapstructure:"set-default-password" default:"false"`
+	AllowMemberTuplesEnabled         bool              `mapstructure:"allow-member-tuples-enabled" default:"false"`
+	IDP                              IDPConfig         `mapstructure:",squash"`
+	Invite                           InviteConfig      `mapstructure:",squash"`
+	Initializer                      InitializerConfig `mapstructure:",squash"`
+	Webhooks                         WebhooksConfig    `mapstructure:",squash"`
 }
 
 func NewConfig() Config {
-	cfg := Config{}
-	cfg.FGA.ObjectType = "core_platform-mesh_io_account"
-	cfg.FGA.ParentRelation = "parent"
-	cfg.FGA.CreatorRelation = "owner"
-	cfg.KCP.Kubeconfig = "/api-kubeconfig/kubeconfig"
-	cfg.APIExportEndpointSliceName = "core.platform-mesh.io"
-	cfg.BaseDomain = "portal.dev.local:8443"
-	cfg.GroupClaim = "groups"
-	cfg.UserClaim = "email"
-	cfg.WorkspacePath = "root"
-	cfg.WorkspaceTypeName = "security"
-	cfg.HttpClientTimeoutSeconds = 30
-	cfg.IDP.KubectlClientRedirectURLs = []string{"http://localhost:8000", "http://localhost:18000"}
-	cfg.IDP.AccessTokenLifespan = 28800
-	cfg.Invite.KeycloakClientID = "security-operator"
-	cfg.Initializer.WorkspaceInitializerEnabled = true
-	cfg.Initializer.IDPEnabled = true
-	cfg.Initializer.InviteEnabled = true
-	cfg.Initializer.WorkspaceAuthEnabled = true
-	cfg.Webhooks.Port = 9443
-	cfg.Webhooks.CertDir = "/tmp/k8s-webhook-server/serving-certs"
-	return cfg
+	return Config{
+		FGA: FGAConfig{
+			ObjectType:      "core_platform-mesh_io_account",
+			ParentRelation:  "parent",
+			CreatorRelation: "owner",
+		},
+		KCP: KCPConfig{
+			Kubeconfig: "/api-kubeconfig/kubeconfig",
+		},
+		APIExportEndpointSliceName: "core.platform-mesh.io",
+		BaseDomain:                 "portal.dev.local:8443",
+		GroupClaim:                 "groups",
+		UserClaim:                  "email",
+		WorkspacePath:              "root",
+		WorkspaceTypeName:          "security",
+		HttpClientTimeoutSeconds:   30,
+		IDP: IDPConfig{
+			KubectlClientRedirectURLs: []string{"http://localhost:8000", "http://localhost:18000"},
+			AccessTokenLifespan:       28800,
+		},
+		Invite: InviteConfig{
+			KeycloakClientID: "security-operator",
+		},
+		Initializer: InitializerConfig{
+			WorkspaceInitializerEnabled: true,
+			IDPEnabled:                  true,
+			InviteEnabled:               true,
+			WorkspaceAuthEnabled:        true,
+		},
+		Webhooks: WebhooksConfig{
+			Port:    9443,
+			CertDir: "/tmp/k8s-webhook-server/serving-certs",
+		},
+	}
 }
 
 func (c *Config) AddFlags(fs *pflag.FlagSet) {
