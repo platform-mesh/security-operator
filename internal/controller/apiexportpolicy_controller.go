528c90c6b17c812629cb3c43d5fdee07f4cace96
OlegErshov <olezhka1629@gmail.com>
2026-05-08 09:34:11 +0200

    chore: remove unused import
    On-behalf-of: SAP aleh.yarshou@sap.com

--- internal/controller/apiexportpolicy_controller.go
+++ internal/controller/apiexportpolicy_controller.go
@@ -30,7 +30,6 @@ import (
 	mcreconcile "sigs.k8s.io/multicluster-runtime/pkg/reconcile"
 
 	"k8s.io/apimachinery/pkg/api/meta"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/kcp-dev/logicalcluster/v3"
